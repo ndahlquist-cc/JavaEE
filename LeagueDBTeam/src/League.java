3a6943ad0d6a325a8a155a005682f74331dc2c23
Nicole Dahlquist <ndahlquist-cc@conestogac.on.ca>
2016-06-20 15:27:28 -0400

    Exercise2

---
+++ LeagueDBTeam/src/League.java
@@ -0,0 +1,40 @@
+/**
+ * Nicole Dahlquist
+ * PROG3060 - Exercise 2
+ * Class definition for the League object in LeagueDB 
+ */
+
+public class League {
+
+	private String leagueID;
+	private String leagueName;
+	private String leagueSponsor;
+	
+	public League() 
+	{
+	}
+	
+	public String getLeagueID() {
+		return leagueID;
+	}
+	
+	public void setLeagueID(String leagueID) {
+		this.leagueID = leagueID;
+	}
+	
+	public String getLeagueName() {
+		return leagueName;
+	}
+	
+	public void setLeagueName(String leagueName) {
+		this.leagueName = leagueName;
+	}
+	
+	public String getLeagueSponsor() {
+		return leagueSponsor;
+	}
+	
+	public void setLeagueSponsor(String leagueSponsor) {
+		this.leagueSponsor = leagueSponsor;
+	}
+}
